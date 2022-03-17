708d6188a50ebd8dfd59d7416b531a50a96f6ef2
GG <weebcyberpunk@gmail.com>
2022-03-17 00:03:14 -0300

    Changed slock foreground logo

--- slock/config.def.h
+++ slock/config.def.h
@@ -14,20 +14,19 @@ static const int failonclear = 1;
 /* insert grid pattern with scale 1:1, the size can be changed with logosize */
 static const int logosize = 75;
 /* grid width and height for right center alignment */
-static const int logow = 12;
+static const int logow = 9;
 static const int logoh = 6;
 
 static XRectangle rectangles[9] = {
 	/* x    y       w       h */
-	{ 0,    3,      1,      3 },
-	{ 1,    3,      2,      1 },
-	{ 0,    5,      8,      1 },
-	{ 3,    0,      1,      5 },
-	{ 5,    3,      1,      2 },
-	{ 7,    3,      1,      2 },
-	{ 8,    3,      4,      1 },
-	{ 9,    4,      1,      2 },
-	{ 11,   4,      1,      2 },
+	{  1,   0,      7,      1 },
+	{  0,   0,      1,      5 },
+	{  1,   4,      7,      1 },
+	{  3,   2,      1,      2 },
+	{  4,   1,      1,      3 },
+	{  2,   2,      1,      1 },
+	{  7,   2,      1,      2 },
+	{  6,   2,      1,      1 },
 };
 
 /*Enable blur*/
